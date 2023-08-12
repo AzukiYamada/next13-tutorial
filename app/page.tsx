50871952bf1c1ba42fd10f6d15ab21c14bac998b
Azuki Yamada <yamada@counterworks.jp>
2023-08-12 23:31:40 +0900

    型定義追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,28 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
+import type { Article } from "./types";
 
-const inter = Inter({ subsets: ["latin"] });
+async function getArticles() {
+  const res = await fetch("http://localhost:3000/api/articles", {
+    cache: "no-store",
+  });
 
-export default function Home() {
+  if (!res.ok) {
+    throw new Error("Failed to fetch articles");
+  }
+
+  const data = await res.json();
+  return data.articles as Article[];
+}
+
+export default async function Home() {
+  const articles = await getArticles();
+
   return (
     <div>
       <h1>新着記事</h1>
       <ul>
-        <li>記事1</li>
-        <li>記事2</li>
-        <li>記事3</li>
+        {articles.map((article) => (
+          <li key={article.id}>{article.title}</li>
+        ))}
       </ul>
     </div>
   );
